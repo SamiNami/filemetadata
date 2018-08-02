a0e3d10b2daeed0eeda634ad6d6d22db02e7bc11
sami peralahti <sami.peralahti@vainu.io>
2018-08-02 13:55:12 -0700

    app works, very simple using the multer package

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 const app = express();
 
@@ -11,6 +12,15 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function(req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+    const { originalname, mimetype, size } = req.file;
+    res.json({
+        name: originalname,
+        type: mimetype,
+        size: size
+    });
 });
 
 app.get('/hello', function(req, res) {
